7a34caa5cd85a7872262ea4f56e86f97bd5275a5
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-07-06 21:12:20 +0100

    implemented linkages

--- models/Typedefs.js
+++ models/Typedefs.js
@@ -1,8 +1,9 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
+  type Query { books: [Book], author: [Author] }
+  type Book { title: String, author: Author }
+  type Author { name: String, age: String }
 `;
 
 
--- resolvers/index.js
+++ resolvers/index.js
@@ -1,11 +1,27 @@
-var books = [{ title: "Legend of tomorrow", author: "Yusuf hassan" },
-{ title: "Who am I?", author: "Jackie Chan" },
-{ title: "The One", author: "Jet li" }]
+var books = [{ title: "Legend of tomorrow", author: 1 },
+{ title: "Who am I?", author: 2 },
+{ title: "The One", author: 2 }]
+
+var author = [
+    { name: "Yusuf hassan", age: 10 },
+    { name: "Jackie Chan", age: 98 },
+    { name: "Jet li", age: 8 }
+]
 
 const resolvers = {
     Query: {
         books: () => books,
+        author: () => author
     },
+    Book: {
+        author(parent) {
+            console.log(parent);
+            return {
+                name: author[parent.author].name,
+                age: author[parent.author].age,
+            };
+        }
+    }
 };
 
 module.exports = resolvers;
