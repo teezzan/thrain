13cf39a17f7f16a25c5c08a059c2df91959c1311
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-07-07 22:11:09 +0100

    added defs formongodb in typedef

--- models/Typedefs.js
+++ models/Typedefs.js
@@ -4,6 +4,32 @@ const typeDefs = gql`
   type Query { books: [Book], author: [Author] }
   type Book { title: String, author: Author }
   type Author { name: String, age: String }
+  type User { 
+              fullname: String, 
+              email:String, 
+              username:String,
+              password: String,
+              verified: Boolean,
+              ideas: [Idea],
+              liked_ideas: [Idea],
+              comments: [Comment]
+   }
+   type Idea { 
+              title: String, 
+              description:String, 
+              author: User,
+              timeCreated: String,
+              tags: [String],
+              likes: Number,
+              comments: [Comment]
+   }
+   type Comment { 
+              text: String, 
+              author: User,
+              timeCreated: String,
+              likes: Number,
+              replies: [Comment]
+   }
 `;
 
 
